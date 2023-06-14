4eb75d8177ccfc7ed3d2f6184e296719e2d0881a
ramesh <raam750@gmail.com>
2023-06-14 20:28:30 +0530

    Dot has been removed.

--- src/Test.java
+++ src/Test.java
@@ -4,6 +4,6 @@ public class Test {
 	public static void main(String[] args) {
 		
 		System.out.println("welcome to java");
-		System.out.println("fileupdated...");
+		System.out.println("fileupdated");
 	}
 }
