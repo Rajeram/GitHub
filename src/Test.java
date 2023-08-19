156e990c79061e71d68f449b7d89d24b70cd0050
ramesh <raam750@gmail.com>
2023-08-19 21:44:36 +0530

    code updated

--- src/Test.java
+++ src/Test.java
@@ -4,9 +4,6 @@ public class Test {
 	public static void main(String[] args) {
 		
 		System.out.println("welcome to java");
-		System.out.println("fileupdated");
-		System.out.println("new line added");
-		System.out.println("file updated with additonal codes");
 		System.out.println("xyz");
 		
 	}
